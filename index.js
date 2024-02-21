c0143d8141fc810036cd3221d911f5ffee59f2cc
Meerata <merata87@gmail.com>
2024-02-21 16:22:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+// writeCards function
+function writeCards(names, eventName) {
+  let thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(
+      `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+
+  return thankYouMessages;
+}
+
+// countDown function
+function countDown(startingNumber) {
+  let currentNumber = startingNumber;
+
+  while (currentNumber >= 0) {
+    console.log(currentNumber);
+    currentNumber--;
+  }
+}
+
+// Export functions if required
+// module.exports = {
+//   writeCards,
+//   countDown
+// };
